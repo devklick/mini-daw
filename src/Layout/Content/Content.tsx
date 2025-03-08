e4a693de6c92383e81f1112b300007e5627668ff
devklick <devklick@gmail.com>
2025-03-08 00:06:58 +0000

    Remove redundant code

--- src/Layout/Content/Content.tsx
+++ src/Layout/Content/Content.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import useAppletManagerStore from "../../stores/useAppletManagerStore";
 
 import "./Content.scss";
@@ -5,9 +6,12 @@ import "./Content.scss";
 function Content() {
   const applets = useAppletManagerStore((s) => s.applets);
 
+  useEffect(() => {
+    console.log("Applets changed", applets);
+  }, [applets]);
   return (
     <main className="content">
-      {Array.from(Object.values(applets)).map((definition) => (
+      {Object.values(applets).map((definition) => (
         <definition.component {...definition.props} key={definition.key}>
           {definition.children}
         </definition.component>
